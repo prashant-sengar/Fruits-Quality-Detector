57e37003a91f55aa1ab3462a6eb0040446fcc51f
Prashant Sengar <prashantsengar812@gmail.com>
2021-08-01 23:22:57 +0530

    Updated settings.py

--- resnet/settings.py
+++ resnet/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$r2od_*!^!npf@1q6l#q=v8!-c=e_kqjij41b_%es$x)e2$#gw'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
